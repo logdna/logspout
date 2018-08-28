d9bea70d2f59c1399d8c9ccfdeafcaaab89b24cc
Samir Musali <samir.musali@logdna.com>
2018-08-28 12:13:28 -0400

    Removing Print Statement

--- logdna/adapter/adapter.go
+++ logdna/adapter/adapter.go
@@ -181,6 +181,5 @@ func buildLogDNAURL(baseURL, token string, tags string) string {
     v.Add("hostname", "logdna_logspout")
 
     ldna_url := "https://" + baseURL + "?" + v.Encode()
-    fmt.Println(ldna_url)
     return ldna_url
 }
