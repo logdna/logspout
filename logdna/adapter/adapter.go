55c394fb529a6c8956f1458828b4e0aac54ce221
Samir Musali <samir.musali@logdna.com>
2019-05-28 14:48:57 -0400

    Please, Work!

--- logdna/adapter/adapter.go
+++ logdna/adapter/adapter.go
@@ -27,16 +27,16 @@ func New(config Configuration) *Adapter {
         LogDNAURL:  buildLogDNAURL(config.Custom.Endpoint, config.Custom.Token),
         Queue:      make(chan Line),
         HTTPClient: &http.Client{
-//            Timeout:    config.HTTPClient.Timeout,
-//            Transport:  &http.Transport{
-//                ExpectContinueTimeout:  config.HTTPClient.ExpectContinueTimeout,
-//                IdleConnTimeout:        config.HTTPClient.IdleConnTimeout,
-//                TLSHandshakeTimeout:    config.HTTPClient.TLSHandshakeTimeout,
-//                DialContext: (&net.Dialer{
-//                    KeepAlive: config.HTTPClient.DialContextKeepAlive,
-//                    Timeout:   config.HTTPClient.DialContextTimeout,
-//                }).DialContext,
-//            },
+            Timeout:    config.HTTPClient.Timeout,
+            Transport:  &http.Transport{
+                ExpectContinueTimeout:  config.HTTPClient.ExpectContinueTimeout,
+                IdleConnTimeout:        config.HTTPClient.IdleConnTimeout,
+                TLSHandshakeTimeout:    config.HTTPClient.TLSHandshakeTimeout,
+                DialContext: (&net.Dialer{
+                    KeepAlive: config.HTTPClient.DialContextKeepAlive,
+                    Timeout:   config.HTTPClient.DialContextTimeout,
+                }).DialContext,
+            },
         },
     }
     go adapter.readQueue()
@@ -200,7 +200,7 @@ func (adapter *Adapter) flushBuffer(buffer []Line) {
         return
     }
 
-    resp, err := adapter.HTTPClient.Post(adapter.LogDNAURL, "application/json; charset=UTF-8", &data)
+    resp, err := http.Post(adapter.LogDNAURL, "application/json; charset=UTF-8", &data)
 
     if resp != nil {
         adapter.Log.Print("Received Status Code: ", resp.StatusCode)
