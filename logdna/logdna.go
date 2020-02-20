7aa7b4f5de432750f907ac62ff3a06071b5af3a6
Samir Musali <samir.musali@logdna.com>
2020-02-20 14:30:21 -0500

    update license in adapter definition

--- logdna/logdna.go
+++ logdna/logdna.go
@@ -4,7 +4,7 @@ package logdna
 // The MIT License (MIT)
 // =====================
 //
-// Copyright (c) 2019 LogDNA, Inc.
+// Copyright (c) 2020 LogDNA, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
